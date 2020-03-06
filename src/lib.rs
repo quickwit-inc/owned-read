e1aa8631f54e894678f62b254defd5e82165d39b
Paul Masurel <paul.masurel@gmail.com>
2020-03-06 09:52:52 +0900

    Removing rental dependency for faster compilation

--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,43 @@
-#[macro_use]
-extern crate rental;
-
-use stable_deref_trait::{CloneStableDeref, StableDeref};
+use stable_deref_trait::StableDeref;
 use std::io;
+use std::mem;
 use std::ops::Deref;
 use std::sync::Arc;
 
-rental! {
-    mod rental_impl {
-        use ::BoxStableDeref;
-
-        #[rental(clone, deref_suffix)]
-        pub(crate) struct OwnedReader {
-            head: BoxStableDeref,
-            data: & 'head [u8],
-        }
-    }
-}
-
-#[derive(Clone)]
-struct BoxStableDeref(Arc<dyn Deref<Target = [u8]>>);
-
-impl BoxStableDeref {
-    fn new<T: Deref<Target = [u8]> + StableDeref + 'static>(inner: T) -> BoxStableDeref {
-        BoxStableDeref(Arc::new(inner))
-    }
-}
-
-unsafe impl StableDeref for BoxStableDeref {}
-unsafe impl CloneStableDeref for BoxStableDeref {}
-
-impl Deref for BoxStableDeref {
-    type Target = [u8];
-
-    fn deref(&self) -> &[u8] {
-        self.0.deref()
-    }
-}
-
 #[derive(Clone)]
 pub struct OwnedRead {
-    inner: rental_impl::OwnedReader,
+    data: &'static [u8],
+    box_stable_deref: Arc<dyn Deref<Target = [u8]>>,
 }
 
 impl OwnedRead {
-    pub fn new<T: StableDeref + Deref<Target = [u8]> + 'static>(data: T) -> OwnedRead {
-        let box_stable_deref = BoxStableDeref::new(data);
-        let inner = rental_impl::OwnedReader::new(box_stable_deref, |arr| arr);
-        OwnedRead { inner }
+    pub fn new<T: StableDeref + Deref<Target = [u8]> + 'static>(data_holder: T) -> OwnedRead {
+        let box_stable_deref = Arc::new(data_holder);
+        let data = unsafe { mem::transmute::<_, &'static [u8]>(box_stable_deref.deref().deref()) };
+        OwnedRead {
+            box_stable_deref,
+            data,
+        }
     }
-
     fn as_slice(&self) -> &[u8] {
-        self.inner.deref()
+        self.data
     }
-
     pub fn is_empty(&self) -> bool {
         self.as_slice().is_empty()
     }
-
     pub fn clip(&mut self, clip_len: usize) {
-        rental_impl::OwnedReader::rent_mut(&mut self.inner, |arr| *arr = &arr[..clip_len]);
+        self.data = &self.data[..clip_len]
     }
-
     pub fn advance(&mut self, advance_len: usize) {
-        rental_impl::OwnedReader::rent_mut(&mut self.inner, |arr| *arr = &arr[advance_len..]);
+        self.data = &self.data[advance_len..]
     }
-
     pub fn slice_from(&self, start: usize) -> &[u8] {
         &self.as_slice()[start..]
     }
-
     pub fn get(&self, idx: usize) -> u8 {
         self.as_slice()[idx]
     }
 }
-
 impl io::Read for OwnedRead {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         let read_len = {
@@ -92,7 +55,6 @@ impl io::Read for OwnedRead {
         self.advance(read_len);
         Ok(read_len)
     }
-
     fn read_to_end(&mut self, buf: &mut Vec<u8>) -> io::Result<usize> {
         let read_len = {
             let data = self.as_slice();
@@ -102,7 +64,6 @@ impl io::Read for OwnedRead {
         self.advance(read_len);
         Ok(read_len)
     }
-
     fn read_exact(&mut self, buf: &mut [u8]) -> io::Result<()> {
         let read_len = self.read(buf)?;
         if read_len != buf.len() {
@@ -114,27 +75,8 @@ impl io::Read for OwnedRead {
         Ok(())
     }
 }
-
 impl AsRef<[u8]> for OwnedRead {
     fn as_ref(&self) -> &[u8] {
         self.as_slice()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::OwnedRead;
-    use std::io::Read;
-
-    #[test]
-    fn test_read() {
-        let mut source_reader = OwnedRead::new((0u8..100u8).collect::<Vec<_>>());
-        let mut buffer = vec![0u8; 5];
-        assert!(source_reader.read_exact(&mut buffer).is_ok());
-        assert_eq!(buffer, &[0u8, 1u8, 2u8, 3u8, 4u8]);
-
-        let mut cloned_reader = source_reader.clone();
-        assert!(cloned_reader.read_exact(&mut buffer).is_ok());
-        assert_eq!(buffer, &[5u8, 6u8, 7u8, 8u8, 9u8]);
     }
 }
